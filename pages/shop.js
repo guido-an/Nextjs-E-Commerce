7b0fa27fe4de5c5998c1f5b08138033e73a5f617
Guido <Guido@mac.local>
2019-05-30 13:08:39 +0200

    test

--- pages/shop.js
+++ pages/shop.js
@@ -6,11 +6,11 @@ const Shop = ({url}) => console.log(vendors.data) || (
   
    <Layout>
 
-    {vendors.data[0].products.map(product => {
+    {/* {vendors.data[0].products.map(product => {
       return <div>
          {product.name}
       </div>
-   })} 
+   })}  */}
 
       <img src={url.query.logo} />
     <h1>{url.query.name}</h1>
