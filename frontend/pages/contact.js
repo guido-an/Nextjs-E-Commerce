107290612d22ca86af3664f9bc170fe2f2688835
Guido Carucci <guido@vanilla.marketing>
2019-10-25 11:45:00 +0200

    Update contact.js

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -2,56 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
 
-// class Contact extends Component {
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value;
-//     const message = document.getElementById("message").value;
-//     console.log(message)
-//     axios({
-//       method: "POST",
-//       url: "http://localhost:5000/send",
-//       data: {
-//         name: name,
-//         email: email,
-//         messsage: message
-//       }
-//     }).then(response => {
-//       if (response.data.msg === "success") {
-//         alert("Message Sent.");
-//         this.resetForm();
-//       } else if (response.data.msg === "fail") {
-//         alert("Message failed to send.");
-//       }
-//     });
-//   };
-
-//   resetForm() {
-//     document.getElementById("contact-form").reset();
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <form id="contact-form" onSubmit={this.handleSubmit}>
-//           <div>
-//             <input type="text" id="name" />
-//           </div>
-//           <div>
-//             <input type="email" id="email" />
-//           </div>
-//           <div>
-//             {/* <textarea id="message" /> */}
-//             <input type="text" id="message" />
-//           </div>
-//           <button>Submit</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
 
 class Contact extends Component {
   state = {
